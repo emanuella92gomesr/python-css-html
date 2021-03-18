4282316e0aace83cbe1533c192caa60eff39f013
Emanuella Gomes <emanuellagomes6@gmail.com>
2021-03-18 20:02:12 +0000

    Fim da parte 1 do curso da Ocean

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,9 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    user = {'username': 'Emanuella'}
+    posts = [
+        {'author': {'username': 'Maria'}, 'body': "Olá da Maria"},
+        {'author': {'username': 'Emanuella'}, 'body': "Olá"}
+    ]
+    return render_template("index.html", user=user, posts=posts)
