0c8f8e38d995e384aa7c42a97f39bc04f07f6f06
Emanuella Gomes <emanuellagomes6@gmail.com>
2021-03-18 22:05:17 +0000

    início da parte 2

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route('/')
 @app.route('/index')
@@ -10,3 +10,9 @@ def index():
         {'author': {'username': 'Emanuella'}, 'body': "Olá"}
     ]
     return render_template("index.html", user=user, posts=posts)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        print(request.values.get("user"), request.values.get("pass"), request.values.get("remember"))
+    return render_template("login.html", title="Login")
