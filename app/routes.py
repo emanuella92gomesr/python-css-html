a445d8b07f837f521390f85f0e23c9ce5bbd4df5
Emanuella Gomes <emanuellagomes6@gmail.com>
2021-03-19 00:02:49 +0000

    Mais um commit da parte 2

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
-from app import app
+from app import app, db
 from flask import render_template, request, redirect, url_for, flash
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
+
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Emanuella'}
     posts = [
@@ -26,3 +28,24 @@ def login():
         login_user(user, remember=r)
         return redirect(url_for('index'))
     return render_template("login.html" , title="Login")
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    if request.method == "POST":
+        user = User(username=request.values.get('user'),
+                    password=request.values.get('pass'))
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template("register.html" , title="Register")
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
